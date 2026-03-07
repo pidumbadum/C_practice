27781b8119a07f4ec853df923f35a1e400b2773a
Alex <sashakasyanova54@gmail.com>
2026-03-07 20:37:19 +0800

    Made Rational class, add operators + += - -=

---
+++ work_with_Rational/Rational.cpp
@@ -0,0 +1,61 @@
+﻿//qwest: 
+//(const Rational& r) что за r и зачем &
+
+#include "Rational.h"
+
+int NOD(int a, int b) {
+	while (b != 0) {
+		int ostatok = a % b;
+		a = b;
+		b = ostatok;
+	}
+	return a;
+}
+
+Rational::Rational()
+{
+	numer = 0;
+	denom = 1;
+}
+Rational::Rational(int namber) {
+	numer = namber;
+	denom = 1;
+}
+Rational::Rational(int num, int den)
+{
+	numer = num;
+	denom = den;
+}
+Rational& Rational::operator += (const Rational& r) {
+	numer = (numer * r.denom + denom * r.numer);
+	denom *= r.denom;
+	return *this;
+}
+Rational Rational::operator + (const Rational& r) const
+{
+	Rational res(*this);
+	return res += r;
+}
+Rational Rational::operator -() const
+{
+	Rational r(-numer, denom);
+	return r;
+}
+//Rational& Rational::operator -(const Rational& r) const
+//{
+//	Rational res(*this);
+//	return (res += (-r));
+//}
+Rational& Rational::operator -=(const Rational& r)
+{
+	return (*this += (-r));
+}
+
+void Rational::simplify() {
+	int nod = NOD(numer, denom);
+	if (nod != 0) {
+		numer /= nod;
+		denom /= nod;
+	}
+	
+}
---
+++ work_with_Rational/Rational.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+class Rational
+{
+private:
+	// упрощение
+	void simplify();
+
+public:
+	int numer;
+	int denom;
+
+	Rational();
+	Rational(int n);
+	Rational(int n, int d);
+
+	Rational& operator += (const Rational& r);
+	// первый & вернет не копию, а ссылку на существующий объект
+	// второй & делает тоже самое, но это надо для экономии памяти 
+	Rational operator + (const Rational& r) const;
+	Rational operator -() const; // const в конце гарантирует, что мы не поменяем 
+								//само число, к которому применяеm сложение, при a + b не должно меняться a.
+	/*Rational operator -(const Rational& r) const;*/
+	Rational& operator -= (const Rational& r); 
+	
+
+};
+

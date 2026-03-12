26932c9547d0488fc348e5db3d1ddbf8feb8792a
Alex <sashakasyanova54@gmail.com>
2026-03-12 13:10:17 +0800

    Create class Array

---
+++ work with vector/vector.cpp
@@ -0,0 +1,37 @@
+﻿#include "vector.h"
+
+Array::Array(int startCapacity)
+{
+	if (startCapacity <= 0)
+		capacity = DEFAULT_CAPACITY;
+	else
+		capacity = startCapacity;
+	ptr = new int[capacity];
+	size = 0;
+}
+Array::~Array()
+{
+	delete[] ptr;
+}
+Array& Array::operator = (const Array& arr)
+{
+	if (this == &arr)
+		return *this; //почему тут указатели? 
+	if (capacity != arr.capacity)
+	{
+		delete[] ptr;
+		ptr = new int[arr.capacity];
+		capacity = arr.capacity;
+	}
+	size = arr.size;
+	for (int i = 0; i < size; i++)
+		ptr[i] = arr.ptr[i];
+	return *this;
+}
+int& Array::operator [](int index) 
+{
+	if (index >= size || index < 0)
+		throw ArrayException();
+	else
+		return ptr[index];
+}
---
+++ work with vector/vector.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+const int DEFAULT_CAPACITY = 10;
+class ArrayException {};
+
+class Array
+{
+private:
+	int* ptr; // Указатель на массив в динамич. памяти
+	int size; // Текущий размер
+	int capacity;// Вместимость
+
+public:
+	explicit Array(int startCapacity = DEFAULT_CAPACITY);
+	~Array();
+
+	Array& operator =(const Array& arr);
+	int& operator [](int index);
+};
+

3fe45fd0d8e1af48606b4ccd5105c67d24a815d6
Alex <sashakasyanova54@gmail.com>
2026-03-23 12:28:54 +0800

    Created classes Graph and Node

---
+++ work-with-Draphs/Graph.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <iostream>
+#include <set>  //вот опять, хоть бы слово о том, что нужна эта библиотека, блин
+#include "Nodes.h"
+using namespace std;
+
+typedef set<Node*>::const_iterator node_iterator;
+
+class Graph {
+private:
+	set<Node*> nodes;
+public:
+	void addNode(Node* node);
+	void removeNode(Node* node);
+	void addEdge(Node* begin, Node* end);
+	void removeEdge(Node* begin, Node* end);
+	node_iterator begin() const {
+		return nodes.begin();
+	}
+	node_iterator end() const { return nodes.end(); }
+};
+
---
+++ work-with-Draphs/Nodes.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <iostream>
+#include <set> 
+
+class Node {
+private:
+	std::string name; 
+	std::set<Node*> neighbours;
+	void addNeighbour(Node* neighbour);
+	void removeNeighbour(Node* neighbour);
+public:
+	Node(const std::string& aname) : name(aname) {}
+	const std::string& getName() const { return name; }
+	node_iterator nb_begin() const {
+		return neighbours.begin();
+	}
+	node_iterator nb_end() const { return neighbours.end(); }
+	friend class Graph;
+};
+

75f21d641d235f43037e37849b9bdef02235c7d3
Alex <sashakasyanova54@gmail.com>
2026-04-02 11:12:38 +0800

    Made data&time class, start with default constructor

---
+++ work-with-Data/data_time.cpp
@@ -0,0 +1,31 @@
+﻿#include "data_time.h"
+
+void find_MY_index(char str[20], int *month_i, int *year_i)
+{
+	int count = 0;
+	for (int i = 0; i < 20; i++) {
+		if (str[i] == ' ' && count < 1) {
+			*month_i = i; //тут я получаю индекс пробела между днем и месяцем
+			count++;
+		}
+		if (str[i] == ' ' && count >= 1) {
+			*month_i = i;
+			count++;
+		}
+	}
+	if (count > 2) { throw Data_TimeException(); }
+}
+
+data_time::data_time()
+{
+	day = 1;
+	month = 1;
+	year = 2001;
+	sec = 0;
+}
+//data_time::data_time(char date[20])
+//{
+//	
+//	
+//
+//}
---
+++ work-with-Data/data_time.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <iostream>
+class Data_TimeException {};
+
+class data_time
+{	
+	int day, month, year, sec;
+public:
+	data_time();
+	data_time(char date[20]);
+	
+
+};
+
